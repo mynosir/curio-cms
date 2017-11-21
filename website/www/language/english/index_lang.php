f70c1a46e56c8cae698e3f3b3394313ead41c385
qoohj <qoohj@qq.com>
2017-11-21 16:14:13 +0800

    update

---
+++ website/www/language/english/index_lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['index_css'] = '.navi{padding: 0 42.5px !important}';
+$lang['index_title']             = 'L & H AUCTION';
+$lang['index_og_description']           = 'L&H Auction Co. Ltd. is the first auction house established by local business in Hong Kong and provides professional service and a new platform in Asia region for discerning collectors.  L&H Auction is specialized in sourcing the exceptional cameras and lens from all over the world, including vintage and special edition cameras, prototypes, lenses and related accessories, from Leica, Carl Zeiss, Hasselblad, Voigtlander, Contax, Rolleiflex, Nikon, Shanghai, Kodar, ALPA, Angenieux, etc..  Mr. James L. Larger, the renowned Leica researcher and historian, is the consultant of the auction.  In addition to paddle bidders, L&H also accepts absentee bidders to make bid via phone, e-mail and fax.  Internet bidders can register online to make bid and participate in the live auction.';
+$lang['index_description'] = 'L&H Auction Co. Ltd. is the first auction house established by local business in Hong Kong and provides professional service and a new platform in Asia region for discerning collectors.  L&H Auction is specialized in sourcing the exceptional cameras and lens from all over the world, including vintage and special edition cameras, prototypes, lenses and related accessories, from Leica, Carl Zeiss, Hasselblad, Voigtlander, Contax, Rolleiflex, Nikon, Shanghai, Kodar, ALPA, Angenieux, etc..  Mr. James L. Larger, the renowned Leica researcher and historian, is the consultant of the auction.  In addition to paddle bidders, L&H also accepts absentee bidders to make bid via phone, e-mail and fax.  Internet bidders can register online to make bid and participate in the live auction.';
+$lang['index_placeholder']         = 'ITEM / NAME';
+$lang['index_navi1'] = 'HOME';
+$lang['index_navi2'] = 'AUCTION';
+$lang['index_navi3'] = 'CATALOGUE';
+$lang['index_navi4'] = 'BUYING & SELLING';
+$lang['index_navi5'] = 'PRESS CENTER';
+$lang['index_navi6'] = 'ABOUT US';
+$lang['index_subnavi1'] = 'UPCOMING AUCTION';
+$lang['index_subnavi2'] = 'PAST AUCTION';
+$lang['index_subnavi3'] = 'CONTACT US';
