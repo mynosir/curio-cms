b9ba4d47ae0084f46d26dba3c5537d31352f1650
linzequan <lowkey361@gmail.com>
2017-12-02 13:28:16 +0800

    添加站内通知功能

--- website/adm/models/notice_model.php
+++ website/adm/models/notice_model.php
@@ -7,54 +7,25 @@
  */
 class Notice_model extends MY_Model {
 
-    private $table = 'curio_notice';
-    private $fields = 'id, content_en, content_tc, url_en, url_tc, sort';
-
     public function __construct() {
         parent::__construct();
+        $this->table = 'curio_notice';
+        $this->fields = 'id, content_en, content_tc, url_en, url_tc, sort';
         $this->loginInfo = $this->session->userdata('loginInfo');
     }
 
 
-
     /**
-     * 新增/更新站内通知
-     * @param [type] $nid    [description]
-     * @param [type] $params [description]
+     * 查询菜单数据
+     * @return [type] [description]
      */
-    public function add($nid, $params) {
-        if($nid == 0) {
-            $this->db->insert($this->table, $params);
-            $result['status'] = 0;
-            $result['msg'] = '保存成功！';
-            return $result;
-        } else {
-            $this->db->where('id', $nid)->update($this->table, $params);
-            $result['status'] = 0;
-            $result['msg'] = '更新成功！';
-            return $result;
-        }
-    }
-
-
-    /**
-     * 分页获取站内通知数据
-     * @param  integer $page    [description]
-     * @param  integer $size    [description]
-     * @param  string  $keyword [description]
-     * @return [type]           [description]
-     */
-    public function getList($page=1, $size=20, $keyword='') {
-        $result = array();
-        if($keyword!='') {
-            $where = ' where title like "%' . $keyword . '%" ';
-        } else {
-            $where = ' where 1=1 ';
-        }
+    public function search($page=1, $size=20) {
         $limitStart = ($page - 1) * $size;
-        $query = $this->db->query('select ' . $this->fields . ' from ' . $this->table . $where . ' order by id desc limit ' . $limitStart . ', ' . $size);
+        $where = ' where 1=1 ';
+        $query = $this->db->query('select ' . $this->fields . ' from ' . $this->table . $where . ' order by `sort` desc limit ' . $limitStart . ', ' . $size);
         $result = $query->result_array();
-        $pageQuery = $this->db->query('select count(1) as num from ' . $this->table . $where);
+
+        $pageQuery = $this->db->query('select count(1) as num from ' . $this->table);
         $pageResult = $pageQuery->result_array();
         $num = $pageResult[0]['num'];
         $rtn = array(
@@ -68,15 +39,93 @@ class Notice_model extends MY_Model {
 
 
     /**
-     * 删除站内通知
+     * 添加数据
+     * @param [type] $params [description]
+     */
+    public function add($params) {
+        $msg = '';
+        if($params['content_en']=='') $msg = '内容（en）不可为空！';
+        if($params['content_tc']=='') $msg = '内容（tc）不可为空！';
+        if($params['url_en']=='') $msg = '链接（en）不可为空！';
+        if($params['url_tc']=='') $msg = '链接（tc）不可为空！';
+
+        if($msg != '') {
+            return array(
+                'status'    => -1,
+                'msg'       => $msg
+            );
+        }
+
+        $data = array(
+            'content_en'    => $params['content_en'],
+            'content_tc'    => $params['content_tc'],
+            'url_en'        => $params['url_en'],
+            'url_tc'        => $params['url_tc'],
+            'sort'          => $params['sort']
+        );
+        $this->db->insert($this->table, $data);
+        return array(
+            'status'    => 0,
+            'msg'       => '操作成功！'
+        );
+    }
+
+
+    /**
+     * 更新数据
+     * @param  [type] $params [description]
+     * @param  [type] $where  [description]
+     * @return [type]         [description]
+     */
+    public function update($params, $where) {
+        $msg = '';
+        if($params['content_en']=='') $msg = '内容（en）不可为空！';
+        if($params['content_tc']=='') $msg = '内容（tc）不可为空！';
+        if($params['url_en']=='') $msg = '链接（en）不可为空！';
+        if($params['url_tc']=='') $msg = '链接（tc）不可为空！';
+
+        if($msg != '') {
+            return array(
+                'status'    => -1,
+                'msg'       => $msg
+            );
+        }
+        $data = array(
+            'content_en'    => $params['content_en'],
+            'content_tc'    => $params['content_tc'],
+            'url_en'        => $params['url_en'],
+            'url_tc'        => $params['url_tc'],
+            'sort'          => $params['sort']
+        );
+        $this->db->where($where)->update($this->table, $data);
+        return array(
+            'status'    => 0,
+            'msg'       => '操作成功！'
+        );
+    }
+
+
+    public function delete($id) {
+        $this->db->delete($this->table, array('id'=> $id));
+        return array(
+            'status'    => 0,
+            'msg'       => '操作成功！'
+        );
+    }
+
+
+    /**
+     * 根据ID查询信息
      * @param  [type] $id [description]
      * @return [type]     [description]
      */
-    public function delete($id) {
-        $this->db->where('id', $id)->delete($this->table);
-        $result['status'] = 0;
-        $result['msg'] = '删除成功';
-        return $result;
+    public function getInfoById($id) {
+        $query = $this->db->query('select ' . $this->fields . ' from ' . $this->table . ' where `id`=' . $id);
+        $result = $query->result_array();
+        return array(
+            'status'    => 0,
+            'msg'       => '操作成功！',
+            'data'      => $result[0]
+        );
     }
-
 }
