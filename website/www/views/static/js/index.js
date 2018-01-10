751aa30c10e94f24f2d7d3dcbbac6c060688051b
qoohj <qoohj@qq.com>
2018-01-10 21:17:52 +0800

    update

--- website/www/views/static/js/index.js
+++ website/www/views/static/js/index.js
@@ -10,7 +10,7 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
     touchRatio: 0.2,
     slideToClickedSlide: true
 });
-galleryTop.params.control = galleryThumbs;
+// galleryTop.params.control = galleryThumbs;
 galleryThumbs.params.control = galleryTop;
 // $(function() {
 //     var page = {
