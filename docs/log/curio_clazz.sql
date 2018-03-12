000be98f680c1217765eb1b413c04bb91ad88a06
qoohj <qoohj@qq.com>
2018-03-12 10:17:22 +0800

    up

--- docs/log/curio_clazz.sql
+++ docs/log/curio_clazz.sql
@@ -1,26 +1,27 @@
-/*
- Navicat Premium Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.20, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: curio
+-- ------------------------------------------------------
+-- Server version	5.7.20
 
- Source Server         : curio
- Source Server Type    : MySQL
- Source Server Version : 50720
- Source Host           : localhost
- Source Database       : curio
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50720
- File Encoding         : utf-8
+--
+-- Table structure for table `curio_clazz`
+--
 
- Date: 12/07/2017 17:59:02 PM
-*/
-
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
---  Table structure for `curio_clazz`
--- ----------------------------
 DROP TABLE IF EXISTS `curio_clazz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `curio_clazz` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `name_en` varchar(255) DEFAULT NULL COMMENT '分类英文',
@@ -29,12 +30,25 @@ CREATE TABLE `curio_clazz` (
   `sort` int(8) DEFAULT '0' COMMENT '排序。数值越大，越靠前',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='内容分类表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `curio_clazz`
--- ----------------------------
-BEGIN;
-INSERT INTO `curio_clazz` VALUES ('1', 'PRESS RELEASE AND PHOTO ALBUM', '新聞稿及相冊', '0', '5'), ('2', 'IN THE NEWS', '媒體報導', '0', '5'), ('10', '2013-06-22 - Auction 1', '2013-06-22 - 第一屆拍賣會', '1', '7'), ('3', 'upcoming auction', '今日拍賣', '0', '5'), ('4', 'buy', '參與拍賣', '0', '5'), ('5', 'sell', '委託拍賣', '0', '5'), ('6', 'about', '公司簡介', '0', '5'), ('7', 'contact', '聯絡我們', '0', '5'), ('11', '2nd', '第2屆', '1', '6'), ('8', 'Terms and Condition of Sale', '條款及細則', '0', '5'), ('9', 'privacy', '個人隱私', '0', '5');
-COMMIT;
+--
+-- Dumping data for table `curio_clazz`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+LOCK TABLES `curio_clazz` WRITE;
+/*!40000 ALTER TABLE `curio_clazz` DISABLE KEYS */;
+INSERT INTO `curio_clazz` VALUES (1,'PRESS RELEASE AND PHOTO ALBUM','新聞稿及相冊',0,5),(2,'IN THE NEWS','媒體報導',0,5),(10,'2013-06-22 - Auction 1','2013-06-22 - 第一屆拍賣會',1,7),(3,'upcoming auction','今日拍賣',0,5),(4,'buy','參與拍賣',0,5),(5,'sell','委託拍賣',0,5),(6,'about','公司簡介',0,5),(7,'contact','聯絡我們',0,5),(11,'2nd','第2屆',1,6),(8,'Terms and Condition of Sale','條款及細則',0,5),(9,'privacy','個人隱私',0,5);
+/*!40000 ALTER TABLE `curio_clazz` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-09 10:29:07
